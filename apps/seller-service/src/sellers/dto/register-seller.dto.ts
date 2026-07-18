2250283d081be82ad3b8b513ff2153d30d988c34
VibeDylan <ton.email@example.com>
2026-07-18 22:19:34 +0200

    doc: fix comment in register seller dto

--- apps/seller-service/src/sellers/dto/register-seller.dto.ts
+++ apps/seller-service/src/sellers/dto/register-seller.dto.ts
@@ -2,8 +2,8 @@ import { IsEmail, IsString } from 'class-validator';
 import { IsSiret } from '../decorators/is-valid-siret.decorator';
 
 /**
- *   Ici je fait une assignation définitive suite a un problème avec mon éditeur qui m'indique 
-*.    une erreur non existante sur mes variables.
+*   Ici je fait une assignation définitive suite a un problème avec mon éditeur qui m'indique 
+*   une erreur non existante sur mes variables.
 */
 
 export class RegisterSellerDto {
