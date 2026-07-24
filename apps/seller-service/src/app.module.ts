b9be1c8eed582f10330f421e2e55c6a6793b99a2
VibeDylan <ton.email@example.com>
2026-07-24 15:48:55 +0200

    chore: add connection to mongodb

--- apps/seller-service/src/app.module.ts
+++ apps/seller-service/src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { SellersModule } from './sellers/sellers.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [SellersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: "../../.env"
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+      }),
+    }),
+    SellersModule,
+  ],
   controllers: [],
   providers: [],
 })
