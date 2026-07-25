55e8a0ca0630e5e0d8973f1f907b4e7b8f8220fe
VibeDylan <ton.email@example.com>
2026-07-25 19:13:03 +0200

    fix: remove forced assigned in register seller dto

--- apps/seller-service/src/sellers/dto/register-seller.dto.ts
+++ apps/seller-service/src/sellers/dto/register-seller.dto.ts
@@ -2,24 +2,19 @@ import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { IsSiret } from '../decorators/is-valid-siret.decorator';
 
-/**
-*   Ici je fait une assignation définitive suite a un problème avec mon éditeur qui m'indique
-*   une erreur non existante sur mes variables.
-*/
-
 export class RegisterSellerDto {
   @ApiProperty({ example: 'acme@example.com' })
   @IsNotEmpty()
   @IsEmail()
-  email!: string;
+  email: string;
 
   @ApiProperty({ example: '73282932000074', description: '14 numeric digits' })
   @IsNotEmpty()
   @IsSiret()
-  siret!: string;
+  siret: string;
 
   @ApiProperty({ example: 'Acme Corp' })
   @IsNotEmpty()
   @IsString()
-  companyName!: string;
+  companyName: string;
 }
