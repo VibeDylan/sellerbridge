9b3bf9d17403f41fc0ad838079101bd32088eae2
VibeDylan <ton.email@example.com>
2026-07-28 16:00:21 +0200

    feat(kyb): add OpenKybCase command

---
+++ apps/kyb-service/src/kyb/commands/open-kyb-case.command.ts
@@ -0,0 +1,9 @@
+import { Command } from '@nestjs/cqrs';
+
+export class OpenKybCaseCommand extends Command<string> {
+  constructor(
+    public readonly sellerId: string,
+  ) {
+    super();
+  }
+}
